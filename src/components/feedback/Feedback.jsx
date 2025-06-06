3b82b5f0c664ffe96e76596ea80872747b2aeacc
Mevlüde Özdemir <mevlde.ozdemir@gmail.com>
2025-06-06 10:27:06 +0300

    updatefeedbak

--- src/components/feedback/Feedback.jsx
+++ src/components/feedback/Feedback.jsx
@@ -1,28 +1,26 @@
 import Notification from "../notification/Notification";
 
 function Feedback({ good, neutral, bad }) {
-    const totalFeedback = good + neutral + bad;
+  const totalFeedback = good + neutral + bad;
 
-    if (totalFeedback === 0) {
+  if (totalFeedback === 0) {
     return <Notification message="No feedback yet." />;
   }
 
-
-    const positivePercentage = totalFeedback > 0 ? Math.round((good / totalFeedback) * 100) : 0;
-
+  const positivePercentage =
+    totalFeedback > 0 ? Math.round((good / totalFeedback) * 100) : 0;
 
   return (
     <div>
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
-       <p><strong>Total: {totalFeedback}</strong></p>
-       <p>Positive: {positivePercentage}%</p>
-
+      <p>
+        <strong>Total: {totalFeedback}</strong>
+      </p>
+      <p>Positive: {positivePercentage}%</p>
     </div>
   );
 }
 
 export default Feedback;
-
-
